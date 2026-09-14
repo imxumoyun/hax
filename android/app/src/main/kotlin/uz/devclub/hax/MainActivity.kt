b87ff26d6946955efb1850677884d85b53f7e3e8
imxumoyun <imxumoyun@gmail.com>
2026-09-14 12:48:12 +0500

    Birinchi sahifa tayyor

---
+++ android/app/src/main/kotlin/uz/devclub/hax/MainActivity.kt
@@ -0,0 +1,5 @@
+package uz.devclub.hax
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFFA259FF),
+        body: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/BG.png"),
+              fit: BoxFit.fill,
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 63),
+              Row(
+                children: [
+                  SizedBox(width: 20),
+                  Image.asset("assets/LOGO.png", width: 100),
+                ],
+              ),
+              Spacer(),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  color: Color(0xFFF6F5F5),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                height: 584,
+                child: Column(
+                  children: [
+                    SizedBox(height: 64),
+
+                    // product icon
+                    Container(
+                      width: 104,
+                      height: 104,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(52),
+                      ),
+                      child: Image.asset("assets/box.png"),
+                    ),
+                    SizedBox(height: 24),
+
+                    // title
+                    Text(
+                      "Non-Contact\nDeliveries",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 34,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    SizedBox(height: 25),
+
+                    // text
+                    Text(
+                      "When placing an order, select the option “Contactless delivery” and the courier will leave your order at the door.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xFF9586A8),
+                      ),
+                    ),
+                    SizedBox(height: 48),
+
+                    // order now button
+                    SizedBox(
+                      width: 374,
+                      height: 56,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF0BCE83),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadiusGeometry.circular(8),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "order now".toUpperCase(),
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+
+                    // dismiss button
+                    TextButton(
+                      onPressed: () {},
+                      child: Text("dismiss".toUpperCase(), style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600, color: Color(0xFF9586A8)),),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
